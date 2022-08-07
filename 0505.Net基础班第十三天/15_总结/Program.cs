8a60c0ff73500d02843b1c6771d3904485fac158
dachunwzc <dachunwzc@outlook.com>
2022-08-07 11:14:57 +0800

    接口小练习

---
+++ 0505.Net基础班第十三天/15_总结/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15_总结
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 什么时候用虚方法来实现多态
+            // 什么时候用抽象类来实现多态
+            // 什么时候用接口来实现多态
+
+            // 真的鸭子会游泳    木头鸭子不会游泳    橡皮鸭子会游泳
+            ISwimming swimming = new RealDuck();
+            swimming.Swimming();
+
+            swimming = new MuDuck();
+            swimming.Swimming();
+
+            swimming = new XPDuck();
+            swimming.Swimming();
+
+            Console.ReadKey();
+        }
+
+        public class RealDuck : ISwimming
+        {
+            public void Swimming()
+            {
+                Console.WriteLine("真的鸭子靠翅膀游泳");
+            }
+        }
+
+        public class MuDuck : ISwimming
+        {
+            public void Swimming()
+            {
+                Console.WriteLine("木鸭子不会游泳");
+            }
+        }
+        
+        public class XPDuck : ISwimming
+        {
+            public void Swimming()
+            {
+                Console.WriteLine("橡皮鸭子飘着游泳");
+            }
+        }
+
+        public interface ISwimming
+        {
+            void Swimming();
+        }
+    }
+}
