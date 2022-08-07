b4dd2bbf5ee90d766118cb298334ce5561bf5d08
dachunwzc <dachunwzc@outlook.com>
2022-08-07 10:39:07 +0800

    第一次提交

---
+++ 0505.Net基础班第十三天/10接口的语法特征/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10接口的语法特征
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
