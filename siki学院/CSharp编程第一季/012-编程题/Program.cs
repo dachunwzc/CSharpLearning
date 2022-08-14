8660647e30be116c98a5b0d6e5ee15575643ac1b
dachunwzc <dachunwzc@outlook.com>
2022-08-14 22:58:53 +0800

    提取百位，十位，个位数字的编程题

---
+++ siki学院/CSharp编程第一季/012-编程题/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _012_编程题
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 提取数字的百位，十位，个位
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int ge = number % 10;
+            int shi = (number / 10) % 10;
+            int bai = (number / 100) % 10;
+
+            Console.WriteLine("number的百位是{0}，十位是{1}，个位是{2}", bai, shi, ge);
+        }
+    }
+}
